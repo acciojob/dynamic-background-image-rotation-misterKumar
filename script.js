421ee7c839772bfb27686cc348a5d57d8d772798
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-02 08:12:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function setScreenOrientationBackground() {
+  const body = document.querySelector('body');
+
+  if (window.innerHeight > window.innerWidth) {
+    body.classList.remove('landscape');
+    body.classList.add('portrait');
+  } else {
+    body.classList.remove('portrait');
+    body.classList.add('landscape');
+  }
+}
+
+// Call the function initially
+setScreenOrientationBackground();
+
+// Listen for screen orientation changes
+window.addEventListener('resize', setScreenOrientationBackground);
